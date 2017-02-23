52279638bf06a16ef128941ac51b5486f78d06f7
Spatterjaaay <aja.pursova@gmail.com>
2017-02-23 12:27:08 -0800

    Added the mehtods for Queues

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,29 @@
 class Queue
   def initialize
+    @store = Array.new
   end
 
   def enqueue(element)
+    @store << element
   end
 
   def dequeue
+    return @store.shift
   end
 
   def front
+    @store.first
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+    return @store.empty?
   end
 
   def to_s
+    return @store.to_s
   end
 end
