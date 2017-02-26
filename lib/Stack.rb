162ec1d0d2d68e3d4a628214e2f32e789462ed43
Spatterjaaay <aja.pursova@gmail.com>
2017-02-26 14:13:42 -0800

    Added methods for stacks

--- lib/Stack.rb
+++ lib/Stack.rb
@@ -4,21 +4,30 @@ class Stack
   end
 
   def push(element)
+    @store.push(element)
   end
 
   def pop
+    return @store.pop
   end
 
   def top
+    return @store.last
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+    # return @store.size == 0
+    # return @store.length == 0
+    return @store.empty?
   end
 
   def to_s
     return @store.to_s
   end
 end
+
+# we implemented a stack using an Array
